e16dd47d11c422ca1a1349584307b550b7f89fc2
prateek041 <prateeksingh9741@gmail.com>
2022-09-13 14:54:07 +0530

    setting up, app.js.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from 'react-router-dom';
+import { Box } from '@mui/material';
 
 const app = () => {
     return (
-        <div>This is test app</div>
+        <Router>
+            <Box sx={{ backgroudColor: '#000' }}>
+                <Navbar />
+                <Routes>
+                    {/* Exact used because react does partial matching, so we use exact to prevent it from happening */}
+                    <Route path='/' exact element={<Feed />} />
+                    <Route path='/video/:id' element={<VideoDetail />} />
+                    <Route path='/channel/:id' element={<ChannelDetail />} />
+                    <Route path='/search/:searchTerm' element={<SearchFeed />} />
+                </Routes>
+            </Box>
+        </Router>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from 'react-router-dom';
 import './index.css';
 
 // importing components.
